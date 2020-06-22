7bc155645b2fc26ba9147e85afcacf19a6851764
sumagnadas <sumagnadas@gmail.com>
2020-06-23 01:57:35 +0530

    Changed the appearance of the website

--- app/main.py
+++ app/main.py
@@ -1,22 +1,28 @@
-from flask import Flask, render_template, stream_with_context, Response
+from flask import Flask, render_template, stream_with_context, Response, request
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', title='HOME PAGE')
+    location = request.args.get('location')
+    if location == "home":
+        return render_template('index.html', title='HOME PAGE', homePressed=True)
+    else:
+        return render_template('index.html', title='HOME PAGE')
 
 #@app.route('/docs')
 #def docs():
 #    return render_template('index.html', title='DOCS')
 
-@app.route("/stream")
+"""@app.route("/stream")
 def stream():
     def generate():
+        yield "<link rel=\"stylesheet\" href=\"static/about.css\">\n<br><span class=\"name\">&gt;&nbsp&nbsp"#<span class=\"labels\">Name</span>\t"
         for i in list("Sumagna Das"):
             yield i
-            for i in range(0, int(20000000 * 0.1)):
+            for i in range(0, int(10000000 * 0.5)):
                 pass
-    return Response(stream_with_context(generate()))
+        yield "</span>"
+    return Response(stream_with_context(generate()))"""
 
 @app.route('/about')
 def about():
