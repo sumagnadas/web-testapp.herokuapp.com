35824528496e11336bce421c732960525c883140
sumagnadas <sumagnadas@gmail.com>
2020-06-22 20:56:04 +0530

    Trying to slowly print my name on the webpage

--- app/main.py
+++ app/main.py
@@ -1,17 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, stream_with_context, Response
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html', title='HOME PAGE')
 
-@app.route('/docs')
-def docs():
-    return render_template('index.html', title='DOCS')
+#@app.route('/docs')
+#def docs():
+#    return render_template('index.html', title='DOCS')
 
 @app.route('/about')
 def about():
-    return render_template('index.html', title='ABOUT')
+    """def generate():
+        for i in list("Sumagna Das"):
+            yield i
+            for i in range(0, int(20000000 * 0.1)):
+                pass
+    return Response(stream_with_context(generate()))"""
+    return render_template("about.html", list=list, sleep=my_sleep)
 
 if __name__ == "__main__":
     app.run()
