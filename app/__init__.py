5e1e64ef287678a004c69a4774905ee01604ddc1
Sumagna Das <48065417+sumagnadas@users.noreply.github.com>
2020-06-29 00:16:12 +0530

    Update __init__.py to include the database URL

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ import os
 
 ADMIN_PASSWORD = 'secret'
 APP_DIR = os.path.dirname(os.path.realpath(__file__))
-DATABASE = 'sqliteext:///%s' % os.path.join(APP_DIR, 'blog.db')
+DATABASE = os.environ['DATABASE_URL']
 DEBUG = False
 SECRET_KEY = 'shhh, secret!'  # Used by Flask to encrypt session cookie.
 SITE_WIDTH = 800
